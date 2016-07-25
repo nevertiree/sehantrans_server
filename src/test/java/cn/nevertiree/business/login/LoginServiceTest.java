d8ac1273ca6f9dbe6e47abebae52ac5ab8311ff4
Lance <nevertiree@126.com>
2016-07-25 19:31:09 +0800

    web可用 加上了MyBatis

---
+++ src/test/java/cn/nevertiree/business/login/LoginServiceTest.java
@@ -0,0 +1,14 @@
+package cn.nevertiree.business.login;
+
+import org.junit.Test;
+
+/**
+ * Created by Lance on 7/25/16.
+ */
+public class LoginServiceTest {
+
+    @Test
+    public void testLoginByPwd(){
+
+    }
+}
