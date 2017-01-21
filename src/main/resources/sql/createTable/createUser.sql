478da193359718bb755551fbfa830a2128263cfc
nevertiree <nevertiree@126.com>
2017-01-21 14:41:11 +0800

    把手机的存储格式改成char(11)，并且把全部的table换成小写

--- src/main/resources/sql/createTable/createUser.sql
+++ src/main/resources/sql/createTable/createUser.sql
@@ -1,7 +1,7 @@
 USE sehantrans;
 
 #记录用于登录的安全信息
-CREATE TABLE IF NOT EXISTS `userLogin` (
+CREATE TABLE IF NOT EXISTS `userlogin` (
   no         CHAR(64)    NOT NULL  PRIMARY KEY ,  #用户id
   username   varchar(32) NOT NULL  UNIQUE ,  #用于登录的名字（通常是邮箱）
   password   varchar(32) NOT NULL
@@ -13,12 +13,12 @@ CREATE TABLE IF NOT EXISTS `userLogin` (
 #用户的个性信息 包括用户头像+用户个人介绍
 #在本地文件系统中存储用户的头像二进制文件和个人介绍长文本，在数据库中记录地址
 #图片的名字、个人简介文字txt的标题内容和用户的ID相同
-CREATE TABLE IF NOT EXISTS userBase(
+CREATE TABLE IF NOT EXISTS userbase(
   no             CHAR(64)       PRIMARY KEY,
   name           VARCHAR(16)    NOT NULL ,             #用户昵称
   gender         ENUM('0','1','2')     DEFAULT '2',   #用户性别:男0女1保密2
   birth          DATETIME       DEFAULT now(),         #用户生日
-  mobile         INT(16)        UNSIGNED,   #用户手机号
+  mobile         CHAR(16)       ,   #用户手机号
   email          VARCHAR(32)    NOT NULL ,             #用户邮箱（注册的时候绑定）
   portrait       VARCHAR(100)   DEFAULT "/var/www/data/portrait/default.png",#用户照片的存储地址
   introduction   VARCHAR(100)   DEFAULT "/var/www/data/introduction/defahult.txt", #用户个人介绍的存储地址
