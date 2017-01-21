4e5268b5f7306a19fd1b5c9d4c57d8f72372fae6
nevertiree <nevertiree@126.com>
2017-01-21 14:40:23 +0800

    用Regex验证手机号和邮箱的可用性

--- src/main/java/cn/nevertiree/common/RegexUtil.java
+++ src/main/java/cn/nevertiree/common/RegexUtil.java
@@ -1,7 +1,22 @@
 package cn.nevertiree.common;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by lance on 17-1-21.
  */
 public class RegexUtil {
+
+    public static boolean correctMobile(String mobile){
+        Pattern pattern = Pattern.compile("^[\\d]{11}$");
+        Matcher matcher = pattern.matcher(mobile);
+        return matcher.find();
+    }
+
+    public static boolean correctEmail(String email){
+        Pattern pattern = Pattern.compile("^[.]*@[.]*\\\\.com$");
+        Matcher matcher = pattern.matcher(email);
+        return matcher.find();
+    }
 }
--- src/test/java/cn/nevertiree/common/RegexUtilTest.java
+++ src/test/java/cn/nevertiree/common/RegexUtilTest.java
@@ -1,8 +1,37 @@
+package cn.nevertiree.common;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by lance on 17-1-21.
  */
 public class RegexUtilTest {
+
+    @Test
+    public void correctEmail() throws Exception {
+        // right
+        assertEquals(true,RegexUtil.correctEmail("nevertiree@wlx.com"));
+    }
+
+
+    @Test
+    public void correctMobile() throws Exception {
+        // right
+        assertEquals(true,RegexUtil.correctMobile("18912345678"));
+        // wrong
+        assertEquals(false,RegexUtil.correctMobile(""));
+        assertEquals(false,RegexUtil.correctMobile("1891234567"));
+        assertEquals(false,RegexUtil.correctMobile("189123456789"));
+        assertEquals(false,RegexUtil.correctMobile("189123456789"));
+        assertEquals(false,RegexUtil.correctMobile("abc"));
+        assertEquals(false,RegexUtil.correctMobile("010"));
+        assertEquals(false,RegexUtil.correctMobile("asdffda"));
+
+    }
 
 }
